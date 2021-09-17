e65a67ab5ef1daac377ead31a9fa38df28dc9d3e
alexandregjr <alexgalocha.jr@gmail.com>
2021-09-17 08:35:16 -0300

    finishing clean up

--- src/config.js
+++ src/config.js
@@ -1,7 +1,3 @@
-//Email e senha Banco
-//project.edux.app@gmail.com
-//projectedu1234
-
 //Firebase configuration
 export const firebaseConfig = {
     apiKey: "AIzaSyAe_LSR3TQ2QJhMsZ_Ft6_S9MMd3r7Goq4",
