655840f8012e305577b493cddecd5b44772121f0
alexandregjr <alexgalocha.jr@gmail.com>
2022-01-18 17:48:03 -0300

    fixing styles of activities/modules screen

--- src/Screens/Activities/Modules.js
+++ src/Screens/Activities/Modules.js
@@ -7,6 +7,7 @@ import { useForm } from 'react-hook-form';
 import axios from 'axios';
 
 import { getAuth } from '@firebase/auth';
+import { Icon } from 'react-native-elements';
 
 function Modules() {
     const navigation = useNavigation();
@@ -42,10 +43,13 @@ function Modules() {
 
         register('search');
         fetch();
-    }, [register, setModules, axios]);
+    }, []);
 
     const makeSearch = async ({ search }) => {
-        if (search === undefined || search === '') return;
+        if (search === undefined || search === '') {
+            setSearchModules(undefined);
+            return;
+        }
 
         setFetching(true);
         axios
@@ -91,9 +95,9 @@ function Modules() {
 
     const subscribedCards = modules?.length
         ? modules.map((mod, index) => (
-              <Card key={mod.id}>
+              <Card containerStyle={styles.card} wrapperStyle={{elevation: 0}} key={mod.id}>
                   <Card.Image
-                      placeholderStyle={{ backgroundColor: '#03a9f4' }}
+                      placeholderStyle={{ backgroundColor: `#${Math.floor(Math.random() * 16777216).toString(16)}` }}
                       style={styles.cardImage}
                       source={{ uri: mod.image ? mod.image : 'a' }}
                   >
@@ -110,49 +114,53 @@ function Modules() {
                                   modName: mod.name
                               });
                           }}
-                          backgroundColor="#03A9F4"
-                          buttonStyle={{
-                              borderRadius: 0,
-                              marginLeft: 0,
-                              marginRight: 0,
-                              marginBottom: 0
-                          }}
+                          buttonStyle={styles.cardButton}
                           title="Visualizar"
                       />
                   </Card.Image>
               </Card>
           ))
-        : null;
+        : 
+        <View style={styles.container}>
+            <Text style={styles.emptyText}>
+                Sem módulos
+            </Text>
+        </View>;
+
+    const cancelSearch = () => {
+        setValue('search', '');
+        setSearchModules(undefined);
+    }
 
     const searchCards = searchModules?.length
         ? searchModules.map((mod, index) => (
-              <Card key={mod.id}>
-                  <Card.Image
-                      placeholderStyle={{ backgroundColor: '#03a9f4' }}
-                      style={styles.cardImage}
-                      source={{ uri: mod.image ? mod.image : 'a' }}
-                  >
-                      <View style={styles.titleWrapper}>
-                          <Card.Title style={styles.cardTitle}>
-                              {mod.name}
-                          </Card.Title>
-                      </View>
-                      <Button
-                          type="solid"
-                          onPress={() => subscribe(mod.id)}
-                          backgroundColor="#03A9F4"
-                          buttonStyle={{
-                              borderRadius: 0,
-                              marginLeft: 0,
-                              marginRight: 0,
-                              marginBottom: 0
-                          }}
-                          title="Se inscrever"
-                      />
-                  </Card.Image>
-              </Card>
+            <Card containerStyle={styles.card} wrapperStyle={{elevation: 0}} key={mod.id}>
+                <Card.Image
+                    placeholderStyle={{ backgroundColor: `#${Math.floor(Math.random() * 16777216).toString(16)}` }}
+                    style={styles.cardImage}
+                    source={{ uri: mod.image ? mod.image : 'a' }}
+                >
+                    <View style={styles.titleWrapper}>
+                        <Card.Title style={styles.cardTitle}>
+                            {mod.name}
+                        </Card.Title>
+                    </View>
+                    <Button
+                        type="solid"
+                        onPress={() => subscribe(mod.id)}
+                        buttonStyle={styles.cardButton}
+                        title="Se inscrever"
+                    />
+                </Card.Image>
+            </Card>
           ))
-        : null;
+        : 
+        <View style={styles.container}>
+            <Text style={styles.emptyText}>
+                Sem resultados
+            </Text>
+        </View>;
+
     return (
         <View>
             <Header
@@ -171,6 +179,7 @@ function Modules() {
                     color: '#fff',
                     onPress: () => navigation.navigate('Perfil')
                 }}
+                containerStyle={{borderBottomWidth: 0}}
             />
             <ScrollView style={styles.scrollBox}>
                 <View style={styles.searchView}>
@@ -182,14 +191,24 @@ function Modules() {
                             setValue('search', text);
                         }}
                     />
+                    {searchModules !== undefined ?
+                    <Button
+                        disabled={fetching}
+                        onPress={cancelSearch}
+                        buttonStyle={styles.cancelButton}
+                        icon={<Icon name="close" color='#fff'/>}
+                    />
+                    :
                     <Button
                         disabled={fetching}
                         onPress={handleSubmit(makeSearch)}
+                        buttonStyle={styles.submitButton}
                         title="Ir"
-                    />
+                    />                    
+                    }
                 </View>
 
-                {searchCards ? searchCards : subscribedCards}
+                {searchModules !== undefined ? searchCards : subscribedCards}
             </ScrollView>
         </View>
     );
@@ -199,17 +218,23 @@ const styles = StyleSheet.create({
     singleLineInput: {
         padding: 8,
         backgroundColor: 'white',
-        borderRadius: 3,
+        borderRadius: 5,
         flexGrow: 1
+    },    
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        paddingTop: 30
     },
     scrollBox: {
         marginBottom: 80
     },
     searchView: {
-        paddingTop: 10,
         flexDirection: 'row',
         alignItems: 'stretch',
-        flex: 1
+        flex: 1,
+        backgroundColor: '#40739e',
+        padding: 15,
     },
     cardTitle: {
         fontSize: 20,
@@ -220,13 +245,52 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
-        flex: 1
+        flex: 1,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
     },
     cardImage: {
-        padding: 10
+        padding: 10,
+        borderRadius: 10,
     },
     cardButton: {
-        backgroundColor: '#eae2b7'
+        backgroundColor: '#40739e',
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+    },
+    submitButton: {
+        borderRadius: 5,
+        backgroundColor: '#6a6',
+        alignSelf: 'stretch',
+        flexGrow: 1,
+        paddingHorizontal: 20,
+        marginLeft: 10
+    },    
+    cancelButton: {
+        borderRadius: 5,
+        backgroundColor: '#d66',
+        alignSelf: 'stretch',
+        flexGrow: 1,
+        paddingHorizontal: 20,
+        marginLeft: 10
+    },
+    emptyText: {
+        color: '#888',
+        fontSize: 16,
+    },
+    card: {
+        shadowColor: '#00000021',
+        shadowOffset: {
+            width: 0,
+            height: 6,
+        },
+        shadowOpacity: 0.37,
+        shadowRadius: 7.49,
+        elevation: 12,
+        borderWidth: 0,
+        borderRadius: 10
     }
 });
 
